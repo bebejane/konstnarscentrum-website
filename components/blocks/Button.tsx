fc1cf02c4f94be4012a4e855d8f84e986c90be67
Bebe Jane <bjornthief@gmail.com>
2025-11-14 21:39:12 +0100

    fix Button invalid urls

--- components/blocks/Button.tsx
+++ components/blocks/Button.tsx
@@ -1,14 +1,13 @@
-import s from './Button.module.scss'
-import React from 'react'
-import Link from 'next/link'
+import s from './Button.module.scss';
+import React from 'react';
+import Link from 'next/link';
 
-export type ButtonBlockProps = { data: ButtonRecord, onClick: Function }
+export type ButtonBlockProps = { data: ButtonRecord; onClick: Function };
 
 export default function Button({ data: { text, url }, onClick }: ButtonBlockProps) {
-
 	return (
-		<Link className={s.button} href={url}>
+		<Link className={s.button} href={url?.trim()}>
 			<button>{text}</button>
 		</Link>
-	)
+	);
 }
